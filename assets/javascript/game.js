ec39e27014baf9a8ec55182937b2c14338f449df
Nick Hegarty <nickhegarty@wifi_guest-10-38-114-76.uncc.edu>
2017-03-13 17:51:00 -0400

    updating

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+
+var targetNumber = 34;
+
+var counter = 0;
+
+var crystalValues = [7, 5, 3, 1];
+
+$("#numberGuess").text(targetNumber);
+
+$(document).ready(function(){
+		Math.floor(Math.random() * 42) + 40; 
+		alert("targetNumber");
+
+	for(var i = 0; i < crystalValues.length; i++) {
+	var crystalImage = $("<img>");
+	crystalImage.addClass("crystal-image");
+	crystalImage.attr("src",)
+	}
+}
+
+
+
