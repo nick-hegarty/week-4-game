4325f2787aa6da22892157c77c7426fcf358a34e
Nick Hegarty <nickhegarty@wifi_guest-10-38-116-88.uncc.edu>
2017-03-15 16:17:52 -0400

    change plan, numbertoGuess works, need to get images to load now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,12 @@ var counter = 0;
 var crystalValues = [7, 5, 3, 1];
 var gem = [];
 
-$("#numberGuess").text(targetNumber);
+targetNumber = Math.floor((Math.random() * 101) + 26);
 
 $(document).ready(function() {
 	function reset(){
-
-		targetNumber = Math.floor((Math.random() * 101) + 26);
+			//CHANGE now I am somehow able to get the randomNumber to work as a global variable, but still can't put images on page....
+		//targetNumber = Math.floor((Math.random() * 101) + 26); //copied exactly how to move data to HTML and this doesn;t error or log...
 		$("#numberGuess").text(targetNumber);
 		console.log(targetNumber);
 
@@ -18,6 +18,7 @@ $(document).ready(function() {
 			crystalImg.attr("data-crystal", crystalValues[i]);
 			crystalImg.attr("src", "http://vignette2.wikia.nocookie.net/zeldafanon/images/5/57/GreenRupee.png/revision/latest?cb=20120501010031");
 			$("#crystals").append(imageCrystal);
+			//nothing is being pushed through, checked links, images, nothing logs...
 		}
 		}
 
@@ -39,7 +40,10 @@ $(document).ready(function() {
 
 	});
 
-console.log("hi there"); //this logs but nothing else comes through
+console.log("hi there");
+$("#numberGuess").text(targetNumber);
+
+ //this logs but nothing else comes through
 
 
 
