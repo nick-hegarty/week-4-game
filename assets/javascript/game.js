8f781d97876b273a9d2c8867009f8d37143009d6
Nick Hegarty <nickhegarty@Nicks-MacBook-Pro.local>
2017-03-14 21:07:31 -0400

    cant load img/values with jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,26 @@
-
-var targetNumber = 34;
-
+var targetNumber;
 var counter = 0;
-
 var crystalValues = [7, 5, 3, 1];
+var gem = [];
 
 $("#numberGuess").text(targetNumber);
 
-$(document).ready(function(){
-		Math.floor(Math.random() * 42) + 40; 
-		alert("targetNumber");
+$(document).ready(function() {
+	function reset(){
 
-	for(var i = 0; i < crystalValues.length; i++) {
-	var crystalImage = $("<img>");
-	crystalImage.addClass("crystal-image");
-	crystalImage.attr("src",)
-	}
-}
+		targetNumber = Math.floor((Math.random() * 101) + 26);
+		$("#numberGuess").text(targetNumber);
+		console.log(targetNumber);
+
+		for (var i = 0; i < crystalValues.length; i++){
+			var crystalImg = $("<img>");
+			crystalImg.addClass("crystal-image");
+			crystalImg.attr("data-crystal", crystalValues[i]);
+			crystalImg.attr("src", "http://vignette2.wikia.nocookie.net/zeldafanon/images/5/57/GreenRupee.png/revision/latest?cb=20120501010031");
+			$("#crystals").append(imageCrystal);
+		}
+		}
+	});
 
 
 
