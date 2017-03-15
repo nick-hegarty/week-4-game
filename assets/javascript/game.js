861b7fa169b1f54b77f6dd833014afa2001fb32b
Nick Hegarty <nickhegarty@wifi_guest-10-38-116-88.uncc.edu>
2017-03-15 15:55:48 -0400

    redoing entire game, trying to get javascript to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 		$("#numberGuess").text(targetNumber);
 		console.log(targetNumber);
 
-		for (var i = 0; i < crystalValues.length; i++){
+		for (var i = 0; i < crystalValues.length; i++){ //attempting to give each gem there own random number from the array from start of game
 			var crystalImg = $("<img>");
 			crystalImg.addClass("crystal-image");
 			crystalImg.attr("data-crystal", crystalValues[i]);
@@ -20,7 +20,26 @@ $(document).ready(function() {
 			$("#crystals").append(imageCrystal);
 		}
 		}
+
+		$(".crystal-image").on("click", function() {
+    
+    var crystalValue = ($(this).attr("data-crystal")); //can't console log ANY data except the test at the bottom.
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+    $("#score").text("New score: " + counter);
+    	if (counter === targetNumber) {
+      		alert("You win!");
+    	}
+   		else if (counter >= targetNumber) {
+      		alert("You lose!!");
+    	}
+ 	console.log(list.targetNumber);
+
+});
+
 	});
+
+console.log("hi there"); //this logs but nothing else comes through
 
 
 
